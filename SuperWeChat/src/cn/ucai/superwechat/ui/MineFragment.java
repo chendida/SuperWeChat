adff776c047667da9606db43fa83d62880359132
chendida <495518592@qq.com>
2017-03-31 12:48:54 +0800

    头像和昵称的显示

--- SuperWeChat/src/cn/ucai/superwechat/ui/MineFragment.java
+++ SuperWeChat/src/cn/ucai/superwechat/ui/MineFragment.java
@@ -2,18 +2,32 @@ package cn.ucai.superwechat.ui;
 
 
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import com.hyphenate.chat.EMClient;
+import com.hyphenate.easeui.domain.User;
+import com.hyphenate.easeui.utils.EaseUserUtils;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 import cn.ucai.superwechat.R;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class MineFragment extends Fragment {
-
+    @BindView(R.id.iv_profile_avatar)
+    ImageView ivProfileAvatar;
+    @BindView(R.id.tv_profile_nickname)
+    TextView tvProfileNickname;
+    @BindView(R.id.tv_profile_username)
+    TextView tvProfileUsername;
 
     public MineFragment() {
         // Required empty public constructor
@@ -23,8 +37,22 @@ public class MineFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_mine, container, false);
+        View view = inflater.inflate(R.layout.fragment_mine, container, false);
+        ButterKnife.bind(this, view);
+        return view;
     }
 
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        initData();
+    }
+
+    private void initData() {
+        String username = EMClient.getInstance().getCurrentUser();
+        User user = EaseUserUtils.getAppUserInfo(username);
+        tvProfileUsername.setText(username);
+        EaseUserUtils.setAppUserAvatar(getContext(),username,ivProfileAvatar);
+        EaseUserUtils.setAppUserNick(username,tvProfileNickname);
+    }
 }
