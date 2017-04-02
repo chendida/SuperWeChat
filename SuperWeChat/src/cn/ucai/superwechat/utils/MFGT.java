4f475e85a6a7e2e713c2676a84392174b03ad2c1
chendida <495518592@qq.com>
2017-04-02 21:36:15 +0800

    标题栏弹窗的点击事件实现以及将添加好友的入口改到弹窗中的添加好友上

--- SuperWeChat/src/cn/ucai/superwechat/utils/MFGT.java
+++ SuperWeChat/src/cn/ucai/superwechat/utils/MFGT.java
@@ -8,6 +8,7 @@ import android.support.v4.app.FragmentActivity;
 import java.util.ArrayList;
 
 import cn.ucai.superwechat.R;
+import cn.ucai.superwechat.ui.AddContactActivity;
 import cn.ucai.superwechat.ui.LoginActivity;
 import cn.ucai.superwechat.ui.MainActivity;
 import cn.ucai.superwechat.ui.RegisterActivity;
@@ -61,5 +62,9 @@ public class MFGT {
 
     public static void gotoUserProfileActivity(Activity activity) {
         startActivity(activity, UserProfileActivity.class);
+    }
+
+    public static void gotoAddContact(Activity activity) {
+        startActivity(activity, AddContactActivity.class);
     }
 }
